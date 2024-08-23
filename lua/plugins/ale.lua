661d4833c9b9beb85433c5b9aff380e6c3bc390a
Alex Yun <alexyun04@gmail.com>
2024-08-22 23:45:58 -0700

    rose pine my beloved

--- lua/plugins/ale.lua
+++ lua/plugins/ale.lua
@@ -1,18 +1,18 @@
 return {
   "dense-analysis/ale",
   config = function()
+    local max_line_length = 90
     -- Configuration goes here.
     local g = vim.g
-
-    g.ale_ruby_rubocop_auto_correct_all = 1
 
     g.ale_linters = {
       lua = { "lua_language_server" },
       python = { "flake8" },
     }
     g.ale_fixers = {
-      python = { "black", "isort" },
+      python = { "black", "isort", "yapf" },
     }
+    g.ale_python_yapf_options = '--style "column_limit: ' .. max_line_length .. '"'
 
     g.ale_fix_on_save = 1
   end,
