6d9751a6259ab034831eca24e1dc40e683f5b935
Alex Yun <alexyun04@gmail.com>
2024-10-03 11:32:32 -0500

    add c, cpp linters and fixers

--- lua/plugins/ale.lua
+++ lua/plugins/ale.lua
@@ -8,11 +8,16 @@ return {
     g.ale_linters = {
       lua = { "lua_language_server" },
       python = { "flake8" },
+      c = { "clangtidy" },
+      cpp = { "clangtidy" },
     }
     g.ale_fixers = {
       python = { "black", "isort", "yapf" },
+      c = { "clang-format" },
+      cpp = { "clang-format" },
     }
     g.ale_python_yapf_options = '--style "column_limit: ' .. max_line_length .. '"'
+    g.ale_c_clangformat_style_options = "--style=Google"
 
     g.ale_fix_on_save = 1
   end,
