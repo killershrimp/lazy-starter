8202791eb862b5b25895c6d48e0adeb8dd103fe9
Alex Yun <alexyun04@gmail.com>
2024-10-19 17:43:03 -0500

    cursor column

--- lua/plugins/notebooknavigator.lua
+++ lua/plugins/notebooknavigator.lua
@@ -26,6 +26,9 @@ return {
   event = "VeryLazy",
   config = function()
     local nn = require("notebook-navigator")
-    nn.setup({ activate_hydra_keys = "<leader>h" })
+    nn.setup({
+      activate_hydra_keys = "<leader>h",
+      syntax_highlight = true,
+    })
   end,
 }
