f9d7248d9c6a4e78dd98f18ddb3be054d33769e9
Alex Yun <alexyun04@gmail.com>
2024-10-21 22:47:23 -0500

    fix c++ lsp

--- lua/plugins/ale.lua
+++ lua/plugins/ale.lua
@@ -8,16 +8,17 @@ return {
     g.ale_linters = {
       lua = { "lua_language_server" },
       python = { "flake8" },
-      c = { "clangtidy" },
-      cpp = { "clangtidy" },
+      c = { "clangd" },
+      cpp = { "clangd" },
     }
     g.ale_fixers = {
       python = { "black", "isort", "yapf" },
-      c = { "clang-format" },
-      cpp = { "clang-format" },
+      c = { "clang-format", "clangtidy" },
+      cpp = { "clang-format", "clangtidy" },
     }
     g.ale_python_yapf_options = '--style "column_limit: ' .. max_line_length .. '"'
     g.ale_c_clangformat_style_options = "--style=Google"
+    g.ale_cpp_clangformat_style_options = "--style=Google"
 
     g.ale_fix_on_save = 1
   end,
