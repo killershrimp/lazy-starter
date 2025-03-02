26259bd97791d4e2b76e80c05d211b504a4c4f7b
Alex Yun <alexyun04@gmail.com>
2025-03-01 21:35:58 -0600

    nvim-cmp, copilot, better vimtex

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>cce", "<cmd>Copilot enable<CR>", { desc = "Enable Copilot" })
+vim.keymap.set("n", "<leader>ccd", "<cmd>Copilot disable<CR>", { desc = "Disable Copilot" })
--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -5,5 +5,14 @@ return {
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = "skim"
+    vim.g.vimtex_complete_enabled = "false"
+    vim.g.vimtex_matchparen_enabled = "false"
+    vim.g.vimtex_imaps_enabled = 0
+    vim.g.vimtex_delim_stopline = 50
+    vim.g.vimtex_quickfix_ignore_filters = {
+      "Underfull \\hbox",
+      "Overfull \\hbox",
+      "LaTeX Warning: .\\+ float specifier changed to",
+    }
   end,
 }
